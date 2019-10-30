4e7b62f094d1c2972cf32725a498caa9c78d97dc
PenzaStreetGames <parassitn@mail.ru>
2019-10-30 23:44:21 +0300

    30.10.2019 3000! графиков зависимостей

--- scripts/mosaicplots.R
+++ scripts/mosaicplots.R
@@ -1,8 +1,12 @@
-j <- "gender"
-for (i in quality_vars) {
-  name <- paste(j, "~", i)
-  t <- table(opros[[j]], opros[[i]])
-  png(paste0(getwd(), "/plots/mosaicplots/", name, ".png"))
-  mosaicplot(t, color = T, shade = T, main = name)
-  dev.off()
+for (i in c(quality_vars, numeric_vars)) {
+  for (j in c(quality_vars, numeric_vars)) {
+    if (i != j) {
+      name <- paste(j, "~", i)
+      t <- table(opros[[j]], opros[[i]])
+      png(paste0(getwd(), "/plots/mosaicplots/", name, ".png"))
+      mosaicplot(t, color = T, shade = T, main = name, xlab = j, ylab = i)
+      dev.off()
+    }
+  }
 }
+mosaicplot(table(opros$gender, opros$languages_number), color = T, shade = T)
